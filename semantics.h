02e97693fcea60bcce7b78a7b41f04eaa615f1cf
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-26 11:24:26 -0500

    Create semantics.h

---
+++ semantics.h
@@ -0,0 +1,15 @@
+#ifndef SEMANTICS_H
+#define SEMANTICS_H
+#include "token.h"
+#include "treeNode.h"
+
+void pushStack(Token tk);
+void popStack(int start);
+bool if_var(Token tk);
+int dist_to_top(Token tk);
+void semantics(Node* node, int count);
+void nodeCheck(Node* node, int count);
+void print_stack();
+void errorFound(std::string string);
+
+#endif
