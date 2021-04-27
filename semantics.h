31365fe0405b2f7642aa95e41d22b35ba411a065
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-26 20:45:10 -0500

    Update semantics.h

--- semantics.h
+++ semantics.h
@@ -8,8 +8,7 @@ void pushStack(Token tk);
 void popStack(int start);
 bool if_var(Token tk);
 int dist_to_top(Token tk);
-void semantics(Node* node, int count);
-void nodeCheck(Node* node, int count);
+void semantics(treeNode* node, int count);
 void print_stack();
 void errorFound(std::string string);
 
