a0b1b04b1048806383c8018fa1099b7a392ae672
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-04-26 21:01:20 -0500

    Update semantics.cpp

--- semantics.cpp
+++ semantics.cpp
@@ -1,212 +1,261 @@
+#include "semantics.h"
 #include <iostream>
 #include <iomanip>
-#include "semantics.h"
 
-#define MAX_SIZE 100
+#define MAX_STACK 100
 
-Token stack[MAX_SIZE];
+Token stack[MAX_STACK];
 
-int variables = 0;
+bool debug1 = false;
+bool debugger2 = true;
+
+int var_count = 0;
 int scope = 0;
 
-void build(){
-    for (int i = 0; i <= MAX_SIZE; i++)
+
+void build_stack(){
+    for (int i = 0; i <= MAX_STACK; i++)
         stack[i].token_string = "";
 }
 
-void pushStack(Token tk) {
-  if(variables < MAX_SIZE) {
-      for(int i = scope; i < variables; i++) {
-          if(stack[i].token_string == tk.token_string) {
-              std::cout << "ERROR: Semantics error detected: " << tk.token_string << " has already been declared in this scope." << std::endl;
-              exit(EXIT_FAILURE);
-          }
-      }
-    
-      stack[variables] = tk;
-      variables++;
-      print_stack();
-  }else {
-      std::cout << "Stack has reached its maximum size of 100 tokens...Exiting" << std::endl;
-      exit(EXIT_FAILURE);
-  }
+void push(Token tk){
+    if (var_count >= MAX_STACK)
+    {
+        std::cout << "Semantics error... Stack has reached size of 100 \nExiting..." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        for (int i = scope; i < var_count; i++)
+        {
+            if (stack[i].token_string == tk.token_string)
+            {
+                std::cout << "Semantics error... \'" << tk.token_string << "\' has already been declared in this scope" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        stack[var_count] = tk; if (debugger2) { std::cout << "Adding \'" << tk.token_string << "\' to the stack\n"; }  // for debugging
+        var_count++; if (debugger2) { std::cout << "VAR COUNT: " << var_count << "\n"; }  // for debugging
+        print_stack();
+    }
 }
 
-void popStack(int start) {
-    for(int i = variables; i > start; i--) {
-        variables--;
+void pop(int scope_begin){
+    for (int i = var_count; i > scope_begin; i--)
+    {
+        var_count--;
         stack[i].token_string == "";
     }
 }
 
-bool if_Var(Token tk) {
-    for(int i = variables - 1; i > -1; i--) {
-        if(stack[i].token_string == tk.token_string)
-            return true;
-    }
-    return false;
-}
-
-int dist_to_top(Token tk) {
-    for(int i = variables; i >= scope; i--) {
-        if(stack[i].token_string == tk.token_string) {
-            int distance = variables - 1 - i;
-            return distance;
+int find(Token tk){
+    for (int i = var_count; i >= scope; i--)
+    {
+        if (stack[i].token_string == tk.token_string)
+        {
+            int tos_distance = var_count - 1 - i;
+            return tos_distance;
         }
     }
+
     return -1;
 }
 
-void semantics(treeNode* node, int count) {
-  
+bool var_exists(Token tk)
+{
+    for (int i = var_count - 1; i > -1; i--)
+    {
+        if (stack[i].token_string == tk.token_string)
+            return true;
+    }
+
+    return false;
+}
+
+void semantic_check(Node* node, int count)
+{
     if (node == nullptr)
         return;
-    if(node->label == "<program>") {
-        int vars = 0;
-        if (node->leaf1 != nullptr)
-            semantics(node->leaf1, vars);
-        if (node->leaf2 != nullptr)
-            semantics(node->leaf2, vars);
-        if (node->leaf3 != nullptr)
-            semantics(node->leaf3, vars);
-        if (node->leaf4 != nullptr)
-            semantics(node->leaf4, vars);
+    if (debug1) {
+        if (node->name == "<vars>" || node->name == "<assign>") { std::cout << "Working on: " << std::setw(10) << std::left << node->name << "| Token: " << std::setw(9) << std::left << node->token_2.token_string << "| "; }  // for debugging
+        else { std::cout << "Working on: " << std::setw(10) << std::left << node->name << "| Token: " << std::setw(9) << std::left << node->token_1.token_string << "| "; }  // for debugging
+        print_stack();
     }
-    else if(node->label == "<vars>") {
-        int distance = dist_to_top(node->token2);
-        scope = variables;
-      
-        if(distance == -1 || distance > count) {
-            pushStack(node->token2);
+    if (node->name == "<program>")
+    {
+        int vars = 0;
+        if (node->child_1 != nullptr)
+            semantic_check(node->child_1, vars);
+        if (node->child_2 != nullptr)
+            semantic_check(node->child_2, vars);
+        if (node->child_3 != nullptr)
+            semantic_check(node->child_3, vars);
+        if (node->child_4 != nullptr)
+            semantic_check(node->child_4, vars);
+    }
+    else if (node->name == "<vars>")
+    {
+        int tos_distance = find(node->token_2);
+        scope = var_count;
+
+        if (tos_distance == -1 || tos_distance > count)
+        {
+            
+            push(node->token_2);
             count++;
         }
-        else if(distance < count) {
-            errorFound(node->token2.token_string);
+        else if (tos_distance < count)
+        {
+            error_declared(node->token_2.token_string);
             exit(EXIT_FAILURE);
         }
-      
-        if(node->leaf1 != nullptr) 
-            semantics(node->leaf1, count);
+
+        if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else if(node->label == "<block>") {
+    else if (node->name == "<block>")
+    {
         unsigned int num_vars = 0;
-        scope = variables;
-      
-        if (node->leaf1 != nullptr)
-            semantics(node->leaf1, num_vars);
-        if (node->leaf2 != nullptr)
-            semantics(node->leaf2, num_vars);
-        if (node->leaf3 != nullptr)
-            semantics(node->leaf3, num_vars);
-        if (node->leaf4 != nullptr)
-            semantics(node->leaf4, num_vars);
-      
-        popStack(scope);
+        scope = var_count;
+
+        if (node->child_1 != nullptr)
+            semantic_check(node->child_1, num_vars);
+        if (node->child_2 != nullptr)
+            semantic_check(node->child_2, num_vars);
+        if (node->child_3 != nullptr)
+            semantic_check(node->child_3, num_vars);
+        if (node->child_4 != nullptr)
+            semantic_check(node->child_4, num_vars);
+
+        pop(scope);
     }
-    else if(node->label == "<expr>") {
-        if(node->token1.tokenID == SUBTRACT_OPERATOR) {
-            if (node->leaf1 != nullptr)
-                semantics(node->leaf1, count);
-            if (node->leaf2 != nullptr)
-                semantics(node->leaf2, count);
-            if (node->leaf3 != nullptr)
-                semantics(node->leaf3, count);
-            if (node->leaf4 != nullptr)
-                semantics(node->leaf4, count);
+    else if (node->name == "<expr>")
+    {
+        if (node->token_1.token_ID == MINUS_TK)
+        {
+            if (node->child_1 != nullptr)
+                semantic_check(node->child_1, count);
+            if (node->child_2 != nullptr)
+                semantic_check(node->child_2, count);
+            if (node->child_3 != nullptr)
+                semantic_check(node->child_3, count);
+            if (node->child_4 != nullptr)
+                semantic_check(node->child_4, count);
         }
-        else if(node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
+        else if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else if(node->label == "<N>") {
-        if(node->token1.tokenID == DIVIDE_OPERATOR || node->token1.tokenID == ASTERISK_OPERATOR) {
-            if (node->leaf1 != nullptr)
-                semantics(node->leaf1, count);
-            if (node->leaf2 != nullptr)
-                semantics(node->leaf2, count);
-            if (node->leaf3 != nullptr)
-                semantics(node->leaf3, count);
-            if (node->leaf4 != nullptr)
-                semantics(node->leaf4, count);
+    else if (node->name == "<N>")
+    {
+        if (node->token_1.token_ID == SLASH_TK || node->token_1.token_ID == ASTERISK_TK)
+        {
+            if (node->child_1 != nullptr)
+                semantic_check(node->child_1, count);
+            if (node->child_2 != nullptr)
+                semantic_check(node->child_2, count);
+            if (node->child_3 != nullptr)
+                semantic_check(node->child_3, count);
+            if (node->child_4 != nullptr)
+                semantic_check(node->child_4, count);
         }
-        else if(node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
+        else if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else if(node->label == "<M>") {
-        if(node->token1.tokenID == ASTERISK_OPERATOR) {
-            if (node->leaf1 != nullptr)
-                semantics(node->leaf1, count);
-            if (node->leaf2 != nullptr)
-                semantics(node->leaf2, count);
-            if (node->leaf3 != nullptr)
-                semantics(node->leaf3, count);
-            if (node->leaf4 != nullptr)
-                semantics(node->leaf4, count);
+    else if (node->name == "<M>")
+    {
+        if (node->token_1.token_ID == ASTERISK_TK)
+        {
+            if (node->child_1 != nullptr)
+                semantic_check(node->child_1, count);
+            if (node->child_2 != nullptr)
+                semantic_check(node->child_2, count);
+            if (node->child_3 != nullptr)
+                semantic_check(node->child_3, count);
+            if (node->child_4 != nullptr)
+                semantic_check(node->child_4, count);
         }
-        else if(node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
+        else if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else if(node->label == "<A>") {
-        if(node->token1.tokenID == ADD_OPERATOR){
-            if (node->leaf1 != nullptr)
-                semantics(node->leaf1, count);
-            if (node->leaf2 != nullptr)
-                semantics(node->leaf2, count);
-            if (node->leaf3 != nullptr)
-                semantics(node->leaf3, count);
-            if (node->leaf4 != nullptr)
-                semantics(node->leaf4, count);
+
+
+
+    else if (node->name == "<A>")
+    {
+        if (node->token_1.token_ID == PLUS_TK)
+        {
+            if (node->child_1 != nullptr)
+                semantic_check(node->child_1, count);
+            if (node->child_2 != nullptr)
+                semantic_check(node->child_2, count);
+            if (node->child_3 != nullptr)
+                semantic_check(node->child_3, count);
+            if (node->child_4 != nullptr)
+                semantic_check(node->child_4, count);
         }
-        else if(node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
+        else if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else if (node->label == "<R>"){
-        if(node->token1.tokenID == IDENTIFIER){
-            if(!if_var(node->token1)){
-                errorFound(node->token1.token_string);
+
+    else if (node->name == "<R>")
+    {
+        if (node->token_1.token_ID == ID_TK)
+        {
+            if (!var_exists(node->token_1))
+            {
+                error_declared(node->token_1.token_string);
                 exit(EXIT_FAILURE);
             }
         }
-        else if (node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
+        else if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else if (node->label == "<in>"){
-        if (!if_var(node->token2)){
-            errorFound(node->token1.token_string);
+    else if (node->name == "<in>")
+    {
+        if (!var_exists(node->token_2))
+        {
+            error_declared(node->token_1.token_string);
             exit(EXIT_FAILURE);
         }
     }
-    else if (node->label == "<assign>"){
-        if (!if_var(node->token2)){
-            errorFound(node->token2.token_string);
+    else if (node->name == "<assign>")
+    {
+        if (!var_exists(node->token_2))
+        {
+            error_declared(node->token_2.token_string);
             exit(EXIT_FAILURE);
         }
-        if (node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
+        if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
     }
-    else {
-        if (node->leaf1 != nullptr)
-            semantics(node->leaf1, count);
-        if (node->leaf2 != nullptr)
-            semantics(node->leaf2, count);
-        if (node->leaf3 != nullptr)
-            semantics(node->leaf3, count);
-        if (node->leaf4 != nullptr)
-            semantics(node->leaf4, count);
+    else
+    {
+        if (node->child_1 != nullptr)
+            semantic_check(node->child_1, count);
+        if (node->child_2 != nullptr)
+            semantic_check(node->child_2, count);
+        if (node->child_3 != nullptr)
+            semantic_check(node->child_3, count);
+        if (node->child_4 != nullptr)
+            semantic_check(node->child_4, count);
     }
 }
 
+void error_declared(std::string tokenString)
+{
+    std::cout << "Semantics error... \'" << tokenString << "\' has not been declared in this scope" << std::endl;
+}
+
+
 void print_stack() {
-    std::cout << "Current Token Stack: ";
-    int count;
-    for (count = 0; count < 100; count++) {
-        std::cout << stack[count].token_string << "";
-        if (stack[count].token_string == "")break;
+    std::cout << "Stack: ";
+    int i;
+    for (i = 0; i < 100; i++) {
+        std::cout << stack[i].token_string << "";
+        if (stack[i].token_string == "")break;
     }
 
     std::cout << std::endl;
-}
-
-void errorFound(std::string string){
-    std::cout << "ERROR: Semantics error found: " << string << " has not been declared in this scope." << std::endl;
 }
     
